64df7f40c41685730153128f9469ababdd0a7747
TapumasGamer <tapumasgamer@gmail.com>
2023-11-26 18:41:59 -0500

    Opciones
    Quedo ya funciona opciones y salir, dentro de opciones hay algunas opciones que podrian ir

--- Assets/Scripts/TuercaManager.cs
+++ Assets/Scripts/TuercaManager.cs
@@ -5,11 +5,16 @@ public class TuercaManager : MonoBehaviour
 {
     private bool pausado = false;
     public GameObject TuercaPanel;
-
+    public GameObject OpcionesPanel;
+    public GameObject MusicaPanel;
+    public GameObject RecursosPanel;
 
     private void Start()
     {
         TuercaPanel.SetActive(false);
+        OpcionesPanel.SetActive(false);
+        MusicaPanel.SetActive(false);
+        RecursosPanel.SetActive(false);
     }
     private void Update()
     {
@@ -30,5 +35,21 @@ public class TuercaManager : MonoBehaviour
     {
         TuercaPanel.SetActive(false);
         pausado= false;
+    }
+    public void Opciones()
+    {
+        OpcionesPanel.SetActive(true);
+        TuercaPanel.SetActive(false);
+        pausado = true;
+    }
+    public void Volver()
+    {
+        OpcionesPanel.SetActive(false);
+        TuercaPanel.SetActive(true);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo de la Aplicación");
     }
 }
